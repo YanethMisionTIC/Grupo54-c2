fe0bdc7cd48b631eb3b38328380e46e47d25471c
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-21 09:26:21 -0500

    Create Exercise7.java

---
+++ RETOS/SEMANA 1/Exercise7.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice1;
+
+/**
+ *Crear un programa que solicite al usuario su nombre y una letra a buscar 
+ *dentro de su nombre. El sistema debe mostrar un mensaje que indique la cantidad de 
+caracteres que tiene su nombre y la cantidad de ocurrencias de la letra que 
+* ingresó. Ejemplo:
+              Nombre: Alejandra
+              Letra a buscar: a
+              Cantidad de ocurrencias: 3
+ * @author YanethM
+ */
+public class Exercise7 {
+    
+}
