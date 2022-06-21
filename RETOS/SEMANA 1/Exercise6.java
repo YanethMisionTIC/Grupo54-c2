e5a728439587521b2c79c8308c337b5bbab1a32f
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-21 09:26:04 -0500

    Create Exercise6.java

---
+++ RETOS/SEMANA 1/Exercise6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice1;
+
+/**
+*Crear un programa que solicita al usuario la contraseña de su wifi, la cual es 
+* “ABC123”, sólo tendrá 3 intentos. Al 3 intento incorrecto, el sistema 
+* le mostrará un mensaje indicando que ha sido bloqueado. Si el ingreso es correcto, 
+* debe solicitarle identificar el número faltante en la sucesión que cumple 
+* con el siguiente patrón:
+* | 2 | 9 | 16 | 23 | 30 | 37 | 44 | Nuevamente solo contará con 3 intentos. 
+* Si el número ingresado es correcto, debe mostrar un mensaje de “Bienvenido al sistema”.
+ * 
+ */
+public class Exercise6 {
+    
+}
