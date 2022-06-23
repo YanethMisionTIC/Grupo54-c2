bd2ea8faee63754c513a5e2326b53bbf0833991e
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-23 06:47:29 -0500

    Create Employee.java

---
+++ SEMANA 2/Sesion 2/Herencia/Employee.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Herencia;
+
+/**
+ *
+ * @author YanethM
+ */
+public class Employee extends Person {
+
+    protected int idEmployee;
+    protected double salary;
+    protected String positionJob;
+    public boolean socialSecurity;
+    //Atributo único de esta clase independiente del tipo de visibilidad
+    protected static int employeeCounter;
+
+    //Caracteristicas del constructor;
+    public Employee(double salary, String positionJob, boolean socialSecurity, String personName, String lastname, String documentType, String document, String email, String cityResidence) {
+        super(personName, lastname, documentType, document, email, cityResidence);
+        this.salary = salary;
+        this.positionJob = positionJob;
+        this.socialSecurity = socialSecurity;
+        //Creamos variable que será el contador de empleados
+        //Cada vez que se cree una instancia de la clase Employee se incrementa en 1 el idEmployee
+        this.idEmployee = ++Employee.employeeCounter;
+    }
+
+    //Clic secundario > insert code > toString()
+    @Override
+    public String toString() {
+        return "Empleado\n{" + "\n            idEmployee: " + idEmployee
+                + "\n            Nombre completo: " + personName + " " + lastname
+                + "\n            Documento: " + documentType + " "
+                + document + ",\n            Ciudad=" + cityResidence
+                + ",\n            Salario: " + salary + ",\n            Cargo: "
+                + positionJob + ",\n            Contacto: " + email + ",\n            ARL:"
+                + socialSecurity + "\n}";
+    }
+
+}
