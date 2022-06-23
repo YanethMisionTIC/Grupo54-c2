a8be1bde3b679154066ef52ea6255afef453df4b
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-23 07:32:24 -0500

    Update Main.java

--- SEMANA 2/Sesion 2/Principal/Main.java
+++ SEMANA 2/Sesion 2/Principal/Main.java
@@ -1,18 +1,23 @@
 package Principal;
 
 import Herencia.*;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner inputDataUser = new Scanner(System.in);
         /*
         =============================================================================
         ===================================Manager==================================
         =============================================================================
          */
-        Manager managerInst = new Manager("Sistemas",4500000, "Contador público", false, "Marco", "de la Puerta", "C.C.", "805310888", "marco@gmail.com", "Bucaramanga");
+        Manager managerInst = new Manager("Sistemas", 4500000, "Contador público", false, "Marco", "de la Puerta", "C.C.", "805310888", "marco@gmail.com", "Bucaramanga");
         System.out.println(managerInst.toString());
+        /*
+        =============================================================================
+        ===================================Operator==================================
+        =============================================================================
+         */
+        Operator operatorInst = new Operator(5, "31/12/2022", 1700000, "Operario de máquina", true, "Hamilton", "Ruiz", "P.E.P.", "716354", "hamilton@gmail.com", "Medellín");
+        System.out.println(operatorInst.toString());
     }
 }
