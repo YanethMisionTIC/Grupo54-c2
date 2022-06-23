1a0972735bd180a544e1aed88318fa3c9a041324
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-23 07:02:24 -0500

    Update Main.java

--- SEMANA 2/Sesion 2/Principal/Main.java
+++ SEMANA 2/Sesion 2/Principal/Main.java
@@ -9,13 +9,10 @@ public class Main {
         Scanner inputDataUser = new Scanner(System.in);
         /*
         =============================================================================
-        ===================================Employee==================================
+        ===================================Manager==================================
         =============================================================================
          */
-        Employee employeeInst = new Employee(2500000, "Ingeniero de sistemas", true, "Jacinto", "Ruiz", "C.C.", "75310888", "jacinto@gmail.com", "Manizales");
-        Employee employeeInst1 = new Employee(4500000, "Contador público", true, "Marco", "de la Puerta", "C.C.", "805310888", "marco@gmail.com", "Bucaramanga");
-        //Imprimimos el objeto que acabamos de crear
-        System.out.println(employeeInst.toString());
-        System.out.println(employeeInst1.toString());
+        Manager managerInst = new Manager("Sistemas",4500000, "Contador público", false, "Marco", "de la Puerta", "C.C.", "805310888", "marco@gmail.com", "Bucaramanga");
+        System.out.println(managerInst.toString());
     }
 }
