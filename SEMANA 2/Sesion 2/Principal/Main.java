5731a29c594ff2d7bf77a7d79e79913861f18b57
YanethMisionTIC <107736403+YanethMisionTIC@users.noreply.github.com>
2022-06-23 07:47:22 -0500

    Update Main.java

--- SEMANA 2/Sesion 2/Principal/Main.java
+++ SEMANA 2/Sesion 2/Principal/Main.java
@@ -19,5 +19,14 @@ public class Main {
          */
         Operator operatorInst = new Operator(5, "31/12/2022", 1700000, "Operario de máquina", true, "Hamilton", "Ruiz", "P.E.P.", "716354", "hamilton@gmail.com", "Medellín");
         System.out.println(operatorInst.toString());
+
+        /*
+        =============================================================================
+        ===================================Correspondent==================================
+        =============================================================================
+         */
+        Correspondent correspondentInst1 = new Correspondent("Moto", 4, "Personal Shopper", true, "Lola", "Mento", "C.C.", "1053888888", "lolamento@gmail.com", "Bogotá");
+        Correspondent correspondentInst2 = new Correspondent("Moto", 4, "Personal Shopper", true, "Alejandro", "Diaz", "C.C.", "1053777777", "alejo@gmail.com", "Bogotá");
+        System.out.println("Corresponsales:\n   [\n" + correspondentInst1.toString() + "\n" + correspondentInst2.toString()+"\n ]");
     }
 }
